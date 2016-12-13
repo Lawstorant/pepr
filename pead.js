ab487a2a94d31f202fd7a06fb165656ad96f2a44
Michał Kopeć <mkopec12@gmail.com>
2016-12-14 00:22:18 +0100

    Removed unnecessary userscript-related leftovers

--- pead.js
+++ pead.js
@@ -1,9 +1,3 @@
-// ==UserScript==
-// @match https://pe.szczecin.pl/chapter_201209.asp
-// ==/UserScript==
-//jshint esversion: 6
-//jshint loopfunc: true
-
 window.onload = function() {
     let a = document.querySelectorAll("ul.aplikacje a.customfont_bold");
     a[0].click();
